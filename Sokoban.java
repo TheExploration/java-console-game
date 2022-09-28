b28f1da8679f260d78f651f4ddf803a0aac0c21f
Exploration <zhangphil@outlook.com>
2022-09-28 00:00:21 -0700

    Add files via upload

--- Sokoban.java
+++ Sokoban.java
@@ -3,20 +3,31 @@ import java.util.Scanner;
 
 
 public class Sokoban {
+        static char[][] ResetBoard = {
+            {'#','#','#','#','#','#','#','#','#'},
+            {'#','#','-','-','-','-','O','-','#'},
+            {'#','#','.','.','.','#','#','#','#'},
+            {'#','#','X','X','X','#','#','#','#'},
+            {'#','#','-','-','-','-','#','#','#'},
+            {'#','#','-','-','-','-','#','#','#'},
+            {'#','#','#','#','#','#','#','#','#'},
+            
+        };
     static char[][] Board = {
-            {'#','#','#','#','#','#','#','#','#','#','#','#'},
-            {'#','#','-','T','-','-','-','-','-','-','#','#'},
-            {'#','#','-','-','-','-','-','X','-','-','#','#'},
-            {'#','#','#','#','#','#','-','-','O','-','#','#'},
-            {'#','#','T','-','-','-','X','-','-','-','#','#'},
-            {'#','#','#','#','#','#','#','#','#','#','#','#'},
+            {'#','#','#','#','#','#','#','#','#'},
+            {'#','#','-','-','-','-','O','-','#'},
+            {'#','#','.','.','.','#','#','#','#'},
+            {'#','#','X','X','X','#','#','#','#'},
+            {'#','#','-','-','-','-','#','#','#'},
+            {'#','#','-','-','-','-','#','#','#'},
+            {'#','#','#','#','#','#','#','#','#'},
             
         };
     static char move;
     static char prev_move;
     static boolean win = false;
-    static int playerY = 3;
-    static int playerX = 8;
+    static int playerY = 1;
+    static int playerX = 6;
     static int moves = 0;
     
     public static void main(String[] args) {
@@ -45,19 +56,21 @@ public class Sokoban {
     System.out.println();
     System.out.println("~~~~~ Welcome to Sokoban ~~~~~\n");
     System.out.println("Puzzle game where you push boxes 'X' to targets 'T'! Player is 'O\n");
-    System.out.println("Controls: w = UP, a = LEFT, s = DOWN, d = RIGHT, q = QUIT\n");
+    System.out.println("Controls: w = UP, a = LEFT, s = DOWN, d = RIGHT, q = QUIT, r = RESTART\n");
     System.out.println("Type in letter and press enter to move. \nOr simply leave blank and just press enter to continue movement.\n");
 
     }
     public static void printGrid() {
-        if (Board[1][3] != 'X' && Board[1][3] != 'O') {
-            Board[1][3] = 'T';
+        if (Board[2][2] != 'X' && Board[2][2] != 'O') {
+            Board[2][2] = '.';
         }
         
-        if (Board[4][2] != 'X' && Board[4][2] != 'O') {
-            Board[4][2] = 'T';
+        if (Board[2][3] != 'X' && Board[2][3] != 'O') {
+            Board[2][3] = '.';
         }
-        
+        if (Board[2][4] != 'X' && Board[2][4] != 'O') {
+            Board[2][4] = '.';
+        }
         
          for (int row = 0; row < Board.length; row++) {
             for (int col = 0; col < Board[row].length; col++) {
@@ -87,19 +100,27 @@ public class Sokoban {
         }
     }
     public static void movePlayer() {
+        if (move == 'r') {
+            System.out.println("Restarted!");
+            Board = ResetBoard;
+            playerY = 1;
+            playerX = 6;
+            moves = 0;
+        }
+        
         if (move == 'q') {
             System.out.println("Game Over. " + "Moves: " + moves);
             System.exit(0);
         }
         if (move == 'a' && Board[playerY][playerX-1] != '#') {
-            if (Board[playerY][playerX-1] == 'X' && (Board[playerY][playerX - 2] == '-' || Board[playerY][playerX - 2] == 'T')) {
+            if (Board[playerY][playerX-1] == 'X' && (Board[playerY][playerX - 2] == '-' || Board[playerY][playerX - 2] == '.')) {
                 Board[playerY][playerX] = '-';
                 Board[playerY][playerX - 1] = 'O';
                 Board[playerY][playerX - 2] = 'X';
                 playerY = playerY;
                 playerX -= 1;
             }
-            if (Board[playerY][playerX-1] == '-' || Board[playerY][playerX-1] == 'T') {
+            if (Board[playerY][playerX-1] == '-' || Board[playerY][playerX-1] == '.') {
                 Board[playerY][playerX] = '-';
                 Board[playerY][playerX-1] = 'O';
                 playerY = playerY;
@@ -107,14 +128,14 @@ public class Sokoban {
             }
         }
         if (move == 'w' && Board[playerY-1][playerX] != '#') {
-            if (Board[playerY-1][playerX] == 'X' && (Board[playerY-2][playerX] == '-' || Board[playerY-2][playerX] == 'T')) {
+            if (Board[playerY-1][playerX] == 'X' && (Board[playerY-2][playerX] == '-' || Board[playerY-2][playerX] == '.')) {
                 Board[playerY][playerX] = '-';
                 Board[playerY-1][playerX] = 'O';
                 Board[playerY-2][playerX] = 'X';
                 playerY -= 1;
                 playerX = playerX;
             }
-            if (Board[playerY-1][playerX] == '-' || Board[playerY-1][playerX] == 'T') {
+            if (Board[playerY-1][playerX] == '-' || Board[playerY-1][playerX] == '.') {
                 Board[playerY][playerX] = '-';
                 Board[playerY-1][playerX] = 'O';
                 playerY -= 1;
@@ -122,14 +143,14 @@ public class Sokoban {
             }
         }
         if (move == 'd' && Board[playerY][playerX+1] != '#') {
-            if (Board[playerY][playerX+1] == 'X' && (Board[playerY][playerX + 2] == '-' || Board[playerY][playerX + 2] == 'T')) {
+            if (Board[playerY][playerX+1] == 'X' && (Board[playerY][playerX + 2] == '-' || Board[playerY][playerX + 2] == '.')) {
                 Board[playerY][playerX] = '-';
                 Board[playerY][playerX + 1] = 'O';
                 Board[playerY][playerX + 2] = 'X';
                 playerY = playerY;
                 playerX += 1;
             }
-            if (Board[playerY][playerX+1] == '-' || Board[playerY][playerX+1] == 'T') {
+            if (Board[playerY][playerX+1] == '-' || Board[playerY][playerX+1] == '.') {
                 Board[playerY][playerX] = '-';
                 Board[playerY][playerX+1] = 'O';
                 playerY = playerY;
@@ -137,14 +158,14 @@ public class Sokoban {
             }
         }
         if (move == 's' && Board[playerY+1][playerX] != '#') {
-            if (Board[playerY+1][playerX] == 'X' && (Board[playerY+2][playerX] == '-' || Board[playerY+2][playerX] == 'T')) {
+            if (Board[playerY+1][playerX] == 'X' && (Board[playerY+2][playerX] == '-' || Board[playerY+2][playerX] == '.')) {
                 Board[playerY][playerX] = '-';
                 Board[playerY+1][playerX] = 'O';
                 Board[playerY+2][playerX] = 'X';
                 playerY += 1;
                 playerX = playerX;
             }
-            if (Board[playerY+1][playerX] == '-' || Board[playerY+1][playerX] == 'T') {
+            if (Board[playerY+1][playerX] == '-' || Board[playerY+1][playerX] == '.') {
                 Board[playerY][playerX] = '-';
                 Board[playerY+1][playerX] = 'O';
                 playerY += 1;
@@ -153,7 +174,7 @@ public class Sokoban {
         }      
     }
     public static void checkWin() {
-        if (Board[1][3] == 'X' && Board[4][2] == 'X') {
+        if (Board[2][2] == 'X' && Board[2][3] == 'X' && Board[2][4] == 'X') {
         win = true;
         
         }
